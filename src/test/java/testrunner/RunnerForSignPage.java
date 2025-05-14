7e537af9a63b8442b5685cdc340b43e78da9e367
Sharan48 <sharanu318@gmail.com>
2025-05-14 17:51:01 +0530

    testng added

--- src/test/java/testrunner/RunnerForSignPage.java
+++ src/test/java/testrunner/RunnerForSignPage.java
@@ -1,13 +1,9 @@
 package testrunner;
 
-import org.junit.runner.RunWith;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/resources/feature/SignPage.feature" }, glue = { "stepdefinitions", "apphooks"
+@CucumberOptions(features = "src/test/resources/feature/SignPage.feature", glue = { "stepdefinitions", "apphooks"
 }, plugin = { "pretty", "json:target/myreport/jsonreport.json" }, monochrome = true)
 public class RunnerForSignPage extends AbstractTestNGCucumberTests {
 
